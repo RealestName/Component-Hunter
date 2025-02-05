92e0acde8165dde3a7ee32e42caff7291f9ce946
RealLev <100689893+RealestName@users.noreply.github.com>
2025-02-05 14:45:11 +0200

    Update fetch_latest_versions.py

--- fetch_latest_versions.py
+++ fetch_latest_versions.py
@@ -5,6 +5,17 @@ import json
 
 GITHUB_FILE_PATH = "script.txt"
 
+# Flask setup
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+def run_flask():
+    """Function to run the Flask app."""
+    app.run(host='0.0.0.0', port=4000)  # Required for Render's web services
+
 # List of libraries and their respective changelog URLs
 library_changelogs = {
     "Next.js": "https://nextjs.org/docs/upgrading",
