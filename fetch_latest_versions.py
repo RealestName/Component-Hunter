a7aff2007aa66a0b946ecff08b916a490076ff61
RealLev <100689893+RealestName@users.noreply.github.com>
2025-02-05 14:57:10 +0200

    Update fetch_latest_versions.py

--- fetch_latest_versions.py
+++ fetch_latest_versions.py
@@ -124,3 +124,9 @@ def update_github_file(file_content, new_versions):
 
     print("GitHub file updated successfully with new versions.")
 
+
+if __name__ == '__main__':
+
+    flask_thread = threading.Thread(target=run_flask)
+    flask_thread.daemon = True
+    flask_thread.start()
